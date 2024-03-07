b8a31e7e664cdfb9989682775fd69fcf782985e4
Yuliia Kachurovska <144264049+yuliakachurovska@users.noreply.github.com>
2024-03-07 18:22:42 +0200

    Update syntax_analyzer.py

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -2,31 +2,23 @@
 # -*- coding: utf-8 -*-
 """
 Модуль призначено для перевірки синтаксичної правильності виразу та присвоєння.
-
 Вираз може мати вигляд:
 #>>> (abc + 123.5)*d2-3/(x+y)
-
 Вираз може містити:
     - змінні - ідентифікатори
     - константи - дійсні або цілі числа без знаку
     - знаки операцій: +, -, *, /
     - дужки: (, )
-
 Присвоєння - це рядок виду
-
 <змінна> = <вираз>
-
 наприклад
 #>>> x = a + b
-
 Функція `check_expression_syntax` за заданим списком токенів
 для виразу має повернути булівське значення та (можливо) помилку
-
 Кожний токен - це кортеж: (<тип токену>, <значення токену>)
 Перевірка робиться на допустимість сусідніх токенів,
 правильний перший та останній токен, порожній вираз,
 правильність розставлення дужок.
-
 Функція `check_assignment_syntax` за заданим списком токенів
 для присвоєння має повернути булівське значення та (можливо) помилку.
 """
@@ -72,6 +64,8 @@ def check_assignment_syntax(tokens):
     """
     if len(tokens) < 3:
         return False, ERRORS["empty_expr"]
+    if _check_start_end(tokens) != True:
+        return False, ERRORS["invalid_start"]
     if tokens[0].type != "variable":
         return False, ERRORS["invalid_start"]
     if tokens[1].type != "equal":
@@ -81,12 +75,9 @@ def check_assignment_syntax(tokens):
 
 def check_expression_syntax(tokens):
     """Функція перевіряє синтаксичну правильність виразу за списком токенів.
-
     Повертає True/False та рядок помилки.
     Якщо помилки немає, то повертає порожній рядок
-
     :param tokens: список токенів типу Token (див. tokenizer.py)
-
     :return:
         success: булівське значення
         error: рядок помилки
@@ -105,7 +96,6 @@ def check_expression_syntax(tokens):
 
 def _check_parens(tokens):
     """Функція перевіряє чи правильно розставлені дужки у виразі.
-
     Повертає True/False.
     :param tokens: список токенів
     :return: success - булівське значення
@@ -124,9 +114,7 @@ def _check_parens(tokens):
 
 def _check_pair(token, next_token):
     """Функція перевіряє чи правильна пара токенів.
-
     Повертає True/False.
-
     :param token: поточний токен
     :param next_token: наступний токен
     :return: success - булівське значення
@@ -137,7 +125,6 @@ def _check_pair(token, next_token):
 def _check_start_end(tokens):
     """Функція перевіряє чи правильний токен стоїть на початку та кінці
     заданого виразу.
-
     :param tokens: список токенів
     :return: success -- True/False
     """
@@ -159,16 +146,21 @@ if __name__ == "__main__":
     success8, error8 = check_assignment_syntax(get_tokens("x + y"))
     success9, error9 = check_assignment_syntax(get_tokens("x ="))
     success10, error10 = check_assignment_syntax(get_tokens("x = (a+b)"))
-
+    success11, error11 = check_assignment_syntax(get_tokens("x = (a = b)"))
+    success12, error12 = check_assignment_syntax(get_tokens("_sdf = ="))
+    success13, error13 = check_expression_syntax(get_tokens("x = x"))
+    
     assert not success1 and error1 == 'Неправильно розставлені дужки'
-    assert not success2 and error2 == "Недопустима пара токенів Token(type='operation', value='*'), Token(type='operation', value='/')"
-    assert not success3 and not success4
-    assert not success5 and error5 == "Недопустима пара токенів Token(type='operation', value='-'), Token(type='other', value='.')"
-    assert not success6 and error6 == "Порожній вираз"
-    assert success7 and error7 == ""
-    assert not success8 and error8 == "Неправильне присвоєння"
+    assert not success2 and error2 == "Недопустима пара токенів Token(type='operation', value='*'), Token(type='operation', value='/')" 
+    assert not success3 and not success4 
+    assert not success5 and error5 == "Недопустима пара токенів Token(type='operation', value='-'), Token(type='other', value='.')" 
+    assert not success6 and error6 == "Порожній вираз" 
+    assert success7 and error7 == "" 
+    assert not success8 and error8 == "Неправильне присвоєння" 
     assert not success9 and error9 == "Порожній вираз"
     assert success10 and error10 == ""
+    assert not success11 and error11 == "Недопустима пара токенів Token(type='variable', value='a'), Token(type='equal', value='=')"
+    assert not success12 and error12 == "Недопустимий початок або кінець"
+    assert not success13 and error13 == "Недопустима пара токенів Token(type='variable', value='x'), Token(type='equal', value='=')"
 
     print("Success =", True)
-
